4af74f297c70d4ff11141b9aa04c0cbe68544117
Jose Castro <31970492+drakonc@users.noreply.github.com>
2020-06-21 22:36:43 -0500

    [MOD] Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { AppComponent } from './app.component';
 
 import { LoginModule } from './layouts/login/login.module';
 import { HomeModule } from './layouts/home/home.module';
-import { FooterComponent } from './layouts/components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, LoginModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent]
